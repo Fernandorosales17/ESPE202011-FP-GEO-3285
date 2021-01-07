9ccaf96b3afa4d128cb6868f5e19a36e56ab728d
unknown <lfernandorosales1712@gmail.com>
2021-01-06 21:50:10 -0500

    workshop 03 operator

---
+++ workshops/Feds 2000/main.java
@@ -0,0 +1,21 @@
+import java.util.scanner;
+
+public class Main{
+ {
+        public static void main(String[ ]args) {
+               Scanner input = new Scanner (system.in) ; 
+               int number = 0 ;
+
+System.out.println(“ indicate a number: ”) ;
+number = input . nextInt( ) ;
+
+if ((number % 2) == 0) {
+             System.out.println( number +  “is even”) ;
+
+else{
+             System.out.println( number +  “is odd”) ;
+                   
+        }
+
+            }
+     } 
