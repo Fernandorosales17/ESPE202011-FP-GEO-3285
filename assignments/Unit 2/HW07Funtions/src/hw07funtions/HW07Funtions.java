82380c3dfdadfbc39f544f3be66cae0df9ec05e1
Fernandorosales17 <lfernandorosales1712@gmail.com>
2021-02-05 00:29:16 -0500

    assignments

---
+++ assignments/Unit 2/HW07Funtions/src/hw07funtions/HW07Funtions.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw07funtions;
+
+/**
+ *
+ * @author Fernando Rosales
+ */
+public class HW07Funtions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+
+    public static float fractionIntegerNumers(int divd, int divr) {
+
+        float quotient = 0.00F;
+        quotient = (float) divd / (float) divr;
+        return (float)quotient;
+    }
+    public static int squareDigit(int base) {
+        int outcome;
+        outcome = base^2;
+        return outcome;
+    }
+    public static int 
+}
+        
+    
+
+
